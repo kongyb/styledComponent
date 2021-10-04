383798ee7ce868818b1e8d15844e64b20253edbe
kongyb <kongyb08@naver.com>
2021-10-04 15:23:12 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+// import styled from 'styled-components';
+import {useRef} from 'react';
+
+// const Button=styled.button`
+//   margin:5px;
+//   font-size:20px;
+//   color:black;
+//   background-color:grey;
+//   border: 1px solid black;
+//   border-radius:5px;
+// `;
+
+// const FixedButton=styled(Button)`
+//   font-weight:bold;
+//   font-family:cursive;
+//   background-color:palegreen;
+// `;
+
+// const CustomButton=styled(Button)`
+//   color:${(props)=>props.color};
+//   background-color:${(props)=>props.background||"gold"};
+// `;
+
+// const Container=styled.div`
+//   text-align:center;
+// `;
+
+function TextInputWithFocusButton() {
+  const inputEl = useRef(null);
+  const onButtonClick = () => {
+    inputEl.current.value='';
+    inputEl.current.focus();
+  };
+  return (
+    <>
+      <input ref={inputEl} type="text" />
+      <button onClick={onButtonClick}>initialize</button>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Container>
+    //   <div><Button>Styled-Button</Button> </div>
+    //   <div><FixedButton>Fixed-Button</FixedButton></div>
+    //   <div><CustomButton color='seashell' background='salmon'>Custom-Button1</CustomButton></div>
+    //   <div><CustomButton color='seashell'>Custom-Button2</CustomButton></div>
+    // </Container>
+    <TextInputWithFocusButton></TextInputWithFocusButton>
   );
 }
 

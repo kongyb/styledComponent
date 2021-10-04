c510eda56acee0df14ec534fa1340d9e41e34fd0
kongyb <kongyb08@naver.com>
2021-10-04 15:23:39 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import './App.css';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import {useRef} from 'react';
 
-// const Button=styled.button`
-//   margin:5px;
-//   font-size:20px;
-//   color:black;
-//   background-color:grey;
-//   border: 1px solid black;
-//   border-radius:5px;
-// `;
+const Button=styled.button`
+  margin:5px;
+  font-size:20px;
+  color:black;
+  background-color:grey;
+  border: 1px solid black;
+  border-radius:5px;
+`;
 
-// const FixedButton=styled(Button)`
-//   font-weight:bold;
-//   font-family:cursive;
-//   background-color:palegreen;
-// `;
+const FixedButton=styled(Button)`
+  font-weight:bold;
+  font-family:cursive;
+  background-color:palegreen;
+`;
 
-// const CustomButton=styled(Button)`
-//   color:${(props)=>props.color};
-//   background-color:${(props)=>props.background||"gold"};
-// `;
+const CustomButton=styled(Button)`
+  color:${(props)=>props.color};
+  background-color:${(props)=>props.background||"gold"};
+`;
 
-// const Container=styled.div`
-//   text-align:center;
-// `;
+const Container=styled.div`
+  text-align:center;
+`;
 
 function TextInputWithFocusButton() {
   const inputEl = useRef(null);
@@ -42,12 +42,12 @@ function TextInputWithFocusButton() {
 
 function App() {
   return (
-    // <Container>
-    //   <div><Button>Styled-Button</Button> </div>
-    //   <div><FixedButton>Fixed-Button</FixedButton></div>
-    //   <div><CustomButton color='seashell' background='salmon'>Custom-Button1</CustomButton></div>
-    //   <div><CustomButton color='seashell'>Custom-Button2</CustomButton></div>
-    // </Container>
+    <Container>
+      <div><Button>Styled-Button</Button> </div>
+      <div><FixedButton>Fixed-Button</FixedButton></div>
+      <div><CustomButton color='seashell' background='salmon'>Custom-Button1</CustomButton></div>
+      <div><CustomButton color='seashell'>Custom-Button2</CustomButton></div>
+    </Container>
     <TextInputWithFocusButton></TextInputWithFocusButton>
   );
 }
